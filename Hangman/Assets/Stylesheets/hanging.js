dc4aec0c903261c1984b83e216cff2047a199f96
airetriton <wobbe_espinoza@hotmail.com>
2017-10-07 21:08:37 -0600

    102 try

--- Hangman/Assets/Stylesheets/hanging.js
+++ Hangman/Assets/Stylesheets/hanging.js
@@ -220,21 +220,7 @@ https://jsfiddle.net/phollott/x29ym2ag/
 
   play();
   
-  // Hint
-
-  //   hint.onclick = function() {
-
-  //     hints = [
-  //       ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-  //       ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-  //       ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
-  //   ];
-
-  //   var catagoryIndex = categories.indexOf(chosenCategory);
-  //   var hintIndex = chosenCategory.indexOf(word);
-  //   showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-  // };
-
+  
    // Reset
 
   document.getElementById('reset').onclick = function() {
